bd29a6f3ef6a065050cfb9053498364db7c0c632
Hirashi3630 <vladayolo.info@gmail.com>
2020-07-01 11:22:13 +0200

    colored print system

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from termcolor import colored
 import json
 import io
 import importlib
@@ -5,16 +7,23 @@ import os
 from pathlib import Path
 
 
+def log(data, color):
+    pre_text = colored("[{0}]:".format(datetime.now().strftime('%H:%M:%S')), 'white')
+    text = colored(data, color)
+    print(pre_text, text)
+
+
 def LoadConfig():
     config_path = 'config.json'
     # check if exists - if not create one
     if os.path.isfile(config_path) and os.access(config_path, os.R_OK):
-        print("Config exists and is readable!")
+        log("Config exists and is readable!", 'green')
     else:
-        print("Either config is missing or is not readable, creating file...")
+        log("Either config is missing or is not readable", 'red')
+        log("creating file...", 'grey')
         with io.open(config_path, 'w') as db_file:
             db_file.write(json.dumps({"modules": [""]}))
-        print("Edit 'config.json' and execute 'main.py' again.")
+        log("Edit 'config.json' and execute 'main.py' again.", 'yellow')
         input("Press Enter to exit...")
         exit()
     # read file if everything is ok
@@ -30,11 +39,11 @@ def CheckModules(data):
         if module_name not in data['modules']:
             break
 
-        print('Found {0} module!'.format(module_name))
+        log('Found {0} module!'.format(module_name), 'green')
 
         new_module = importlib.import_module("Modules." + module_name)
         new_module.Start()
-        print('{0} finished!'.format(module_name))
+        log('{0} finished!'.format(module_name), 'green')
 
 
 CheckModules(LoadConfig())
