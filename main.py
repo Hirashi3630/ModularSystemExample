b6ad81162517d9eac880ed518d936ea54997100a
Hirashi3630 <vladayolo.info@gmail.com>
2020-07-01 11:17:19 +0200

    refactoring

--- main.py
+++ main.py
@@ -4,31 +4,37 @@ import importlib
 import os
 from pathlib import Path
 
-# config
-CONFIG_PATH = 'config.json'
-# check if exists - if not create one
-if os.path.isfile(CONFIG_PATH) and os.access(CONFIG_PATH, os.R_OK):
-    print("Config exists and is readable!")
-else:
-    print("Either config is missing or is not readable, creating file...")
-    with io.open(CONFIG_PATH, 'w') as db_file:
-        db_file.write(json.dumps({"modules": [""]}))
-    print("Edit 'config.json' and execute 'main.py' again.")
-    input("Press Enter to exit...")
-    exit()
-# read file if everything is ok
-with open(CONFIG_PATH) as config_file:
-    data = json.load(config_file)
 
-# check /Modules folder
-for path in Path('Modules').rglob('*.py'):
+def LoadConfig():
+    config_path = 'config.json'
+    # check if exists - if not create one
+    if os.path.isfile(config_path) and os.access(config_path, os.R_OK):
+        print("Config exists and is readable!")
+    else:
+        print("Either config is missing or is not readable, creating file...")
+        with io.open(config_path, 'w') as db_file:
+            db_file.write(json.dumps({"modules": [""]}))
+        print("Edit 'config.json' and execute 'main.py' again.")
+        input("Press Enter to exit...")
+        exit()
+    # read file if everything is ok
+    with open(config_path) as config_file:
+        data = json.load(config_file)
+    return data
 
-    module_name = os.path.splitext(path.name)[0]  # most complicated way lol
-    if module_name not in data['modules']:
-        break
 
-    print('Found {0} module!'.format(module_name))
+def CheckModules(data):
+    for path in Path('Modules').rglob('*.py'):
 
-    new_module = importlib.import_module("Modules." + module_name)
-    new_module.Start()
-    print('{0} finished!'.format(module_name))
+        module_name = os.path.splitext(path.name)[0]  # most complicated way lol
+        if module_name not in data['modules']:
+            break
+
+        print('Found {0} module!'.format(module_name))
+
+        new_module = importlib.import_module("Modules." + module_name)
+        new_module.Start()
+        print('{0} finished!'.format(module_name))
+
+
+CheckModules(LoadConfig())
